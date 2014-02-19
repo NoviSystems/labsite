47e6772ee6e1f38a0644bb41508a401a6bf84a95
Ian Chiles <ianrchiles@gmail.com>
2014-02-19 11:04:28 -0500

    fix logout redirect weirdness

--- labsite/urls.py
+++ labsite/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -9,7 +11,9 @@ urlpatterns = patterns(
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login', name='login'),
-    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', name='logout'),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': "/accounts/login"}, name="logout"),
     url(r'^lunch/', include('foodapp.urls')),
     url(r'^worklog/', include('worklog.urls')),
 )
+
+urlpatterns += staticfiles_urlpatterns()
