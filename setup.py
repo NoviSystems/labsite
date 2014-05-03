dc3e2332cd570084e55e4c7553414aaa1fba1f86
Ryan P Kilby <rpkilby@ncsu.edu>
2014-05-03 14:21:16 -0400

    Manifest file for asset files

--- setup.py
+++ setup.py
@@ -5,18 +5,12 @@ def read(fname):
   return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "foodapp",
-    version = "0.1",
-    author = "OSCAR Lab",
-    packages = find_packages(),
+    name="foodapp",
+    version="0.1",
+    author="OSCAR Lab",
+    packages=find_packages(),
+    include_package_data=True,
     # long_description=read('README.md'),
-    package_data = {
-        'foodapp': [
-            'fixtures/*',
-            'static/*',
-            'templates/*',
-        ],
-    },
     install_requires=[
         "django>=1.5",
         "south",
