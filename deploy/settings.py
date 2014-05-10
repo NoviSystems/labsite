034b23bdf7ae2757a21102158e8e41df48328bca
Will Refvem <wbrefvem@ncsu.edu>
2014-05-09 23:42:53 -0400

    Changed path to static root

--- deploy/settings.py
+++ deploy/settings.py
@@ -109,8 +109,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/opt/lab/labsite/foodapp/foodapp/static/",
-    "/opt/lab/labsite/worklog/foodapp/static/",
+    "/opt/lab/labsite/static-root/",
 )
 
 # List of finder classes that know how to find static files in
