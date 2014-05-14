c49daa79e751e1fcb15c4346d4e82082f4ac52e4
Will Refvem <wbrefvem@ncsu.edu>
2014-05-14 11:37:44 -0400

    Removed stray print statement

--- labsite/views.py
+++ labsite/views.py
@@ -5,5 +5,4 @@ class LabsiteView(TemplateView):
 	template_name = 'labsite.html'
 
 	def get(self, request, *args, **kwargs):
-		print "executed view"
 		return super(LabsiteView, self).get(request, *args, **kwargs)
