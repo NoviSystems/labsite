2e5a42a429d2ac59e8cea421b9e0e4b4bbe69a07
Meaghan Johnson <mejohn10@ncsu.edu>
2014-07-17 11:07:04 -0400

    qpid configs

--- labsite/settings.ex.py
+++ labsite/settings.ex.py
@@ -3,6 +3,8 @@ from core_settings import *
 
 SECRET_KEY = 'xz#=4k+4we__68ge_i6v_e5v^+km*p+j=+8qjcq(ox%qle!@k*'
 
+# Celery broker URL
+# BROKER_URL = 'qpid://qpid-1.oscar.ncsu.edu:5672/'
 
 DATABASES = {
     'default': {
