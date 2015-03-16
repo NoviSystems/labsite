3be0822dd1411e1ddfceab831ae7cb7babe9f277
Ryan P Kilby <rpkilby@ncsu.edu>
2015-03-16 14:45:34 -0400

    Pulled in provision fixes

--- labsite/fabfile/provision.py
+++ labsite/fabfile/provision.py
@@ -19,7 +19,7 @@ IP_ADDR = re.compile(r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$')
 
 
 __all__ = [
-    'application', 'broker', 'database', 'devel', 'all',
+    'application', 'frontend', 'worker', 'broker', 'database', 'devel', 'all',
 ]
 
 
@@ -85,6 +85,12 @@ def frontend():
     execute(config.certify)
     sudo('setsebool -P httpd_read_user_content 1')
 
+    # We need to give the nginx server exec access to the labuser's home
+    # directory. This is necessary so nginx has access to the static-root
+    # and media-root directories.
+    sudo('usermod -a -G labuser nginx')
+    sudo('chmod g+x ~labuser')
+
 
 @task
 @roles('application')
@@ -106,6 +112,12 @@ def broker():
 
     require.service.started('redis')
     require.service.enabled('redis')
+
+    # TODO: formalize this a la postgres
+    # require.redis.listening_on('0.0.0.0')
+    files.sed('/etc/redis.conf', r'^(\s*)?bind.*$', 'bind 0.0.0.0', use_sudo=True)
+    files.append('/etc/redis.conf', 'bind 0.0.0.0', use_sudo=True)
+    require.service.restarted('redis')
 
 
 @task
