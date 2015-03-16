ebd5ff0ea2443bce8ae034ef51dcbc7c98c6ba2d
Ryan P Kilby <rpkilby@ncsu.edu>
2015-03-16 14:50:56 -0400

    Updating db settings again

--- labsite/settings_stag.py
+++ labsite/settings_stag.py
@@ -8,13 +8,10 @@ TEMPLATE_DEBUG = DEBUG
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'lab_stag',  # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'lab_stag',
         'USER': 'lab_stag',
-        'PASSWORD': '',
-        'HOST': 'postgres92.oscar.ncsu.edu',  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',  # Set to empty string for default.
+        'HOST': 'lab-db.oscar.priv',
     }
 }
 
