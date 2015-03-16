ea1171f8d44f771b48e70907bb0d5d54d9ead311
Ryan P Kilby <rpkilby@ncsu.edu>
2015-03-16 14:45:03 -0400

    Updated db settings

--- labsite/settings_prod.py
+++ labsite/settings_prod.py
@@ -16,9 +16,9 @@ TEMPLATE_DEBUG = DEBUG
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'prod_lab',  # Or path to database file if using sqlite3.
+        'NAME': 'lab_prod',  # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'prod_lab',
+        'USER': 'lab_prod',
         'PASSWORD': '',
         'HOST': 'postgres92.oscar.ncsu.edu',  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',  # Set to empty string for default.
