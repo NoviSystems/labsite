92a8ca78c8c80eeb3fb653d16d73438966d4683f
Ryan P Kilby <rpkilby@ncsu.edu>
2015-03-25 13:17:07 -0400

    Update celery

--- foodapp/tasks.py
+++ foodapp/tasks.py
@@ -1,12 +1,11 @@
-import time
-from celery.task import task, periodic_task
-from celery.schedules import crontab
+
+from celery import shared_task
 from foodapp.models import RiceCooker
 
-@periodic_task(run_every=crontab(hour=0,minute=1,day_of_week=[0,1,2,3,4,5,6]))
-def resetRiceCooker():
+@shared_task
+def reset_rice_cooker():
 	cookers = RiceCooker.objects.all()
 	for cooker in cookers: 
 		cooker.is_on = False
 		cooker.save()
-	
+
