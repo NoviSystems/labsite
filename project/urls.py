d115f2c98f88ec89db2c7e16201d9c4b83867eae
Dylan Stein <djstein@ncsu.edu>
2015-04-20 15:33:35 -0400

    Created a site wide nav bar. Implemeted a new navigation bar by changing labsite.html to base.html, creating a context processor, and establishing a dictionary of installed apps in settings_core.py.

--- project/urls.py
+++ project/urls.py
@@ -1,15 +1,15 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from views import LabsiteView
+from views import HomepageView
 
 
 urlpatterns = [
-    url(r'^$', LabsiteView.as_view()),
+    url(r'^$', HomepageView.as_view()),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login', name='login'),
     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': "/accounts/login"}, name="logout"),
-    url(r'^lunch/', include('foodapp.urls')),
-    url(r'^worklog/', include('worklog.urls')),
+    url(r'^lunch/', include('foodapp.urls', app_name='foodapp')),
+    url(r'^worklog/', include('worklog.urls', app_name='worklog')),
 ]
