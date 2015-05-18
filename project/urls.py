5309671740255f1fad3a36dfb4597e58031a69fb
dralley <dralley@ncsu.edu>
2015-05-18 13:09:26 -0400

    style formatting

--- project/urls.py
+++ project/urls.py
@@ -6,10 +6,14 @@ from views import LabsiteView
 
 urlpatterns = [
     url(r'^$', LabsiteView.as_view()),
+
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
+
     url(r'^accounts/login/$', 'django.contrib.auth.views.login', name='login'),
     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': "/accounts/login"}, name="logout"),
+
+    # App urls
     url(r'^lunch/', include('foodapp.urls')),
     url(r'^worklog/', include('worklog.urls')),
 ]
--- project/views.py
+++ project/views.py
@@ -2,7 +2,7 @@ from django.views.generic.base import TemplateView
 
 
 class LabsiteView(TemplateView):
-	template_name = 'labsite.html'
+    template_name = 'labsite.html'
 
-	def get(self, request, *args, **kwargs):
-		return super(LabsiteView, self).get(request, *args, **kwargs)
+    def get(self, request, *args, **kwargs):
+        return super(LabsiteView, self).get(request, *args, **kwargs)
