5682569569dcc486ce359548e22be07594fb6c12
Dylan Stein <djstein@ncsu.edu>
2016-02-10 14:16:56 -0500

    Added LoginRequirementsMixin

--- accounting/views.py
+++ accounting/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.core.urlresolvers import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 # Create your views here.
-class TestView(TemplateView):
+class TestView(LoginRequiredMixin, TemplateView):
     template_name = 'accounting/home.html'
     success_url = reverse_lazy('accounting-home')
 
