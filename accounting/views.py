5aeb027a668daa09245cc8de4be7c8b2bd540727
Dylan Stein <djstein@ncsu.edu>
2016-02-10 15:03:42 -0500

    Home page now displays available business units

--- accounting/views.py
+++ accounting/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.core.urlresolvers import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
+from models import *
+from django.contrib.auth.models import User
 
 
 # Create your views here.
@@ -11,4 +13,6 @@ class TestView(LoginRequiredMixin, TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(TestView, self).get_context_data()
+        business_units = BusinessUnit.objects.filter(user=self.request.user)
+        context['business_units'] = business_units
         return context
