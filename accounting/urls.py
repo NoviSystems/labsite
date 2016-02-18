59ccff9f4f09ef0359f8d1705567e6f1482d1257
Dylan Stein <djstein@ncsu.edu>
2016-02-18 16:14:12 -0500

    changed ordering of param values in all urls to before 'name' of usage

--- accounting/urls.py
+++ accounting/urls.py
@@ -4,25 +4,25 @@ from accounting.views import *
 urlpatterns = [
 	url(r'^$', HomePageView.as_view(), name='home'),
 
-	url(r'^dashboard/(?P<pk>\d+)/$', DashboardView.as_view(), name='dashboard'),
-
 	url(r'^create_business_unit/$', BusinessUnitCreateView.as_view(), name='create_business_unit'),
-	url(r'^update_business_unit/(?P<business_unit>\d+)/$', BusinessUnitUpdateView.as_view(), name='update_business_unit'),
-	url(r'^delete_business_unit/(?P<business_unit>\d+)/$', BusinessUnitDeleteView.as_view(), name='delete_business_unit'),
+	url(r'^(?P<business_unit>\d+)/update_business_unit/$', BusinessUnitUpdateView.as_view(), name='update_business_unit'),
+	url(r'^(?P<business_unit>\d+)/delete_business_unit/$', BusinessUnitDeleteView.as_view(), name='delete_business_unit'),
 
-	url(r'^dashboard/(?P<pk>\d+)/create_fiscal_year/$', FiscalYearCreateView.as_view(), name='create_fiscal_year'),
-	url(r'^dashboard/(?P<pk>\d+)/update_fiscal_year/(?P<fiscal_year>\d+)/$', FiscalYearUpdateView.as_view(), name='update_fiscal_year'),
-	url(r'^dashboard/(?P<pk>\d+)/delete_fiscal_year/(?P<fiscal_year>\d+)/$', FiscalYearDeleteView.as_view(), name='delete_fiscal_year'),
+	url(r'^(?P<pk>\d+)/dashboard/$', DashboardView.as_view(), name='dashboard'),
 
-	url(r'^dashboard/(?P<pk>\d+)/create_expense/(?P<month>\d+)/$', ExpenseCreateView.as_view(), name='create_expense'),
-	url(r'^dashboard/(?P<pk>\d+)/update_expense/(?P<expense>\d+)/$', ExpenseUpdateView.as_view(), name='update_expense'),
-	url(r'^dashboard/(?P<pk>\d+)/delete_expense/(?P<expense>\d+)/$', ExpenseDeleteView.as_view(), name='delete_expense'),
+	url(r'^(?P<pk>\d+)/dashboard/create_fiscal_year/$', FiscalYearCreateView.as_view(), name='create_fiscal_year'),
+	url(r'^(?P<pk>\d+)/dashboard/(?P<fiscal_year>\d+)/update_fiscal_year/$', FiscalYearUpdateView.as_view(), name='update_fiscal_year'),
+	url(r'^(?P<pk>\d+)/dashboard/(?P<fiscal_year>\d+)/delete_fiscal_year/$', FiscalYearDeleteView.as_view(), name='delete_fiscal_year'),
 
-	url(r'^contracts/(?P<pk>\d+)/$', ContractsView.as_view(), name='contracts'),
+	url(r'^(?P<pk>\d+)/dashboard/(?P<month>\d+)/create_expense/$', ExpenseCreateView.as_view(), name='create_expense'),
+	url(r'^(?P<pk>\d+)/dashboard/(?P<expense>\d+)/update_expense/$', ExpenseUpdateView.as_view(), name='update_expense'),
+	url(r'^(?P<pk>\d+)/dashboard/(?P<expense>\d+)/delete_expense/$', ExpenseDeleteView.as_view(), name='delete_expense'),
 
-	url(r'^contracts/(?P<pk>\d+)/create_contract/$', ContractCreateView.as_view(), name='create_contract'),
-	url(r'^contracts/(?P<pk>\d+)/update_contract/(?P<contract>\d+)/$', ContractUpdateView.as_view(), name='update_contract'),
-	url(r'^contracts/(?P<pk>\d+)/delete_contract/(?P<contract>\d+)/$', ContractDeleteView.as_view(), name='delete_contract'),
+	url(r'^(?P<pk>\d+)/contracts/$', ContractsView.as_view(), name='contracts'),
 
-	url(r'^expenses/(?P<pk>\d+)/$', ExpensesView.as_view(), name='expenses'),
+	url(r'^(?P<pk>\d+)/contracts/create_contract/$', ContractCreateView.as_view(), name='create_contract'),
+	url(r'^(?P<pk>\d+)/contracts/(?P<contract>\d+)/update_contract/$', ContractUpdateView.as_view(), name='update_contract'),
+	url(r'^(?P<pk>\d+)/contracts/(?P<contract>\d+)/delete_contract/$', ContractDeleteView.as_view(), name='delete_contract'),
+
+	url(r'^(?P<pk>\d+)/expenses/$', ExpensesView.as_view(), name='expenses'),
 ]
