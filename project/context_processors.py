629cef2376c9c9d8a1cb21147d59a5f81fd0bf7c
Ryan P Kilby <rpkilby@ncsu.edu>
2016-03-10 21:21:29 -0500

    Fix current_app resolution is navbar

--- project/context_processors.py
+++ project/context_processors.py
@@ -1,8 +1,9 @@
+
 from django.conf import settings
 
 
 def navbar_context(request):
     return {
-      'current_app': request.resolver_match.app_name,
-      'labsite_apps': settings.LABSITE_APPS,
+        'current_app': getattr(request.resolver_match, 'app_name', None),
+        'labsite_apps': settings.LABSITE_APPS,
     }
