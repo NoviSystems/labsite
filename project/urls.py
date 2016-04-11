57eab161bd735dbd0ac6a95f0f44bb793eb36d40
Daniel Alley <dralley@ncsu.edu>
2016-04-11 19:07:15 +0000

    Removed string view arguments in url() to fix Django 1.10 deprecation warning

--- project/urls.py
+++ project/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import login as auth_login, logout as auth_logout
 from django.conf.urls import include, url
 from django.contrib import admin
 
@@ -10,8 +11,8 @@ urlpatterns = [
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^accounts/login/$', 'django.contrib.auth.views.login', name='login'),
-    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/accounts/login'}, name='logout'),
+    url(r'^accounts/login/$', auth_login, name='login'),
+    url(r'^accounts/logout/$', auth_logout, {'next_page': '/accounts/login'}, name='logout'),
     url(r'^accounts/', include('itng.registration.auth_urls')),
     url(r'^accounts/', include('itng.registration.backends.invite.urls.activation')),
 
