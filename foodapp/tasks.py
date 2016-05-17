e6bd9b40f0aa4c46df6096b279b00e4602104590
Daniel Alley <dralley@ncsu.edu>
2016-05-17 20:24:02 +0000

    Finished email code, but still needs to be tested

--- foodapp/tasks.py
+++ foodapp/tasks.py
@@ -1,26 +1,22 @@
 import stripe
-from datetime import datetime
 
 from celery import shared_task
 from django.conf import settings
 from django.core import mail
-from django.core.mail import EmailMessage, EmailMultiAlternatives
 from django.template import Template, Context
 
 from foodapp.models import RiceCooker, StripeCustomer
-
-from django.contrib.auth.models import User
 
 stripe.api_key = settings.STRIPE_API_KEY
 
 
 notification_email_msg = Template("""
 This is your friendly reminder to submit payment for your
-recent Foodapp invoice for a total of {{ total }}.  Please
+recent Foodapp invoice for a total of {{ total_cost }}.  Please
 navigate to the following link in order to view and pay this
 invoice.
 
-{{ payment_url }}
+{% url 'foodapp:stripe_invoice_create' %}
 
 If you fail to pay this invoice, the items will be rolled into
 the next invoice.
@@ -56,24 +52,30 @@ def create_invoices():
     customers = StripeCustomer.objects.all()
 
     if settings.FOODAPP_SEND_INVOICE_REMINDERS:
-
         for customer in customers:
-            invoice = stripe.Invoice.create(customer=customer.customer_id)
+            # Calculate total cost
+            all_invoice_items = stripe.InvoiceItem.all(customer=customer.customer_id)
 
+            total_cost = 0
+            for data in all_invoice_items.get('data'):
+                if data['invoice'] is None:
+                    total_cost += int(data['amount'])
+
+            total_cost = '$%.2f' % (total_cost / 100.00)
+
+            # Send email
             user = customer.user
+            subj = "[Foodapp] Payment required - you have unpaid items on Foodapp"
+            from_email = settings.DEFAULT_FROM_EMAIL
+            recipients = [user.email]
 
-            if not invoice.closed:
-                subj = "[Foodapp] Payment required - you have unpaid items on Foodapp"
-                from_email = settings.DEFAULT_FROM_EMAIL
-                recipients = [user.email]
+            msg = notification_email_msg.render(Context({"total_cost": total_cost}))
+            email = mail.EmailMessage(subj, msg, from_email, recipients)
 
-                msg = notification_email_msg.render(Context({"total": 0, "payment_url": ""}))
-                email = mail.EmailMessage(subj, msg, from_email, recipients)
+            # html_msg = html_email_msg.render(Context())
+            # email = mail.EmailMultiAlternatives(subj, msg, from_email, recipients)
+            # email.attach_alternative(html_msg, 'text/html')
 
-                # html_msg = html_email_msg.render(Context())
+            email.connection = connection
+            email.send()
 
-                # email = mail.EmailMultiAlternatives(subj, msg, from_email, recipients)
-                # email.attach_alternative(html_msg, 'text/html')
-
-                email.connection = connection
-                email.send()
