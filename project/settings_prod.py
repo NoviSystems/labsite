9a4b511bb514d6df196bfee2f93e90fcd1eb8a8a
Daniel Alley <dralley@ncsu.edu>
2016-10-11 13:10:41 -0400

    Changed task name

--- project/settings_prod.py
+++ project/settings_prod.py
@@ -46,7 +46,7 @@ CELERYBEAT_SCHEDULE = {
         'schedule': crontab(hour=0, minute=1, day_of_week=[0, 1, 2, 3, 4, 5, 6]),
     },
     'weekly_billing': {
-        'task': 'foodapp.tasks.send_invoice_notifications',
+        'task': 'foodapp.tasks.create_invoices_and_send_notifications',
         'schedule': crontab(hour=0, minute=0, day_of_week=[6]),
     },
 }
