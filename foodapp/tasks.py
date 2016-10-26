83552f965f0de582bbfc2ce1f8e8e1ecbb45f9df
Ryan P Kilby <rpkilby@ncsu.edu>
2016-10-26 19:34:48 -0400

    Update tasks

--- foodapp/tasks.py
+++ foodapp/tasks.py
@@ -1,16 +1,12 @@
-import stripe
 
 from celery import shared_task
 from django.conf import settings
-from django.core import mail
 from django.core.urlresolvers import reverse
 from django.template import Template, Context
 
 from foodapp import models
 
-stripe.api_key = settings.STRIPE_API_SECRET_KEY
-
-notification_email_msg = Template("""
+PAYMENT_REQUIRED_BODY = Template("""
 This is your friendly reminder to submit payment for your
 recent Foodapp invoice for a total of {{ total_cost }}.  Please
 navigate to the following link in order to view and pay this
@@ -29,56 +25,36 @@ def reset_rice_cooker():
 
 
 @shared_task
-def create_invoices_and_send_notifications():
+def create_invoices():
+    for customer in models.StripeCustomer.objects.all():
+        customer.user.orders.generate_invoice_items()
 
+
+@shared_task
+def send_invoice_notifications():
     customers = models.StripeCustomer.objects.all()
 
-    # Create invoices
+    # Ensure invoice items exist
     for customer in customers:
-        uninvoiced_orders = models.Order.objects.filter(user=customer.user, is_invoiceable=True, invoice_item=None)
+        customer.user.orders.generate_invoice_items()
 
-        for order in uninvoiced_orders:
-            customer_id = customer.customer_id
+    # Send reminders after generation in case email sending errors
+    if not settings.FOODAPP_SEND_INVOICE_REMINDERS:
+        return
 
-            order.invoiceitem = stripe.InvoiceItem.create(
-                customer=customer_id,
-                # amount must be in cents
-                amount=(int(100 * order.item.cost * order.quantity)),
-                currency="usd",
-                description=order.item.name,
-                metadata={"quantity": order.quantity, "date": order.date},
-            )
-            order.save()
+    for customer in customers:
+        uninvoiced_items = customer.get_uninvoiced_items()
 
-    # Send reminders
-    if settings.FOODAPP_SEND_INVOICE_REMINDERS:
-        connection = mail.get_connection(fail_silently=False)
+        # skip if no need to notify user
+        if len(uninvoiced_items) == 0:
+            continue
 
-        for customer in customers:
-            # Calculate total cost
-            all_invoice_items = stripe.InvoiceItem.all(customer=customer.customer_id).get('data')
-            total_cost = 0
+        total_cost = sum(item.amount for item in uninvoiced_items)
 
-            if len(all_invoice_items) == 0:
-                continue
-
-            for data in all_invoice_items:
-                if data['invoice'] is None:
-                    total_cost += int(data['amount'])
-
-            total_cost = '$%.2f' % (total_cost / 100.00)
-            url = settings.SITE_URL + reverse('foodapp:stripe_invoices')
-
-            # Send email
-            user = customer.user
-            subj = "[Foodapp] Payment required - you have unpaid items on Foodapp"
-            from_email = settings.DEFAULT_FROM_EMAIL
-            recipients = [user.email]
-
-            msg = notification_email_msg.render(
-                Context({"total_cost": total_cost, "url": url})
-            )
-            email = mail.EmailMessage(subj, msg, from_email, recipients)
-
-            email.connection = connection
-            email.send()
+        # Send email
+        subject = "[Foodapp] Payment required - you have unpaid items on Foodapp"
+        message = PAYMENT_REQUIRED_BODY.render(Context({
+            'total_cost': '$%.2f' % (total_cost / 100.00),
+            'url': settings.SITE_URL + reverse('foodapp:stripe_invoices')
+        }))
+        customer.user.email_user(subject, message)
