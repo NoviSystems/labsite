e6041421218a42259bfe028fad34c790831ec39b
Ryan P Kilby <rpkilby@ncsu.edu>
2017-02-06 14:53:10 -0500

    Drop old expense/cash/etc... views

--- accounting/urls.py
+++ accounting/urls.py
@@ -30,23 +30,6 @@ urlpatterns = [
     url(r'^(?P<business_unit>\d+)/update/$', views.BusinessUnitUpdateView.as_view(), name='update_business_unit'),
     url(r'^(?P<business_unit>\d+)/delete/$', views.BusinessUnitDeleteView.as_view(), name='delete_business_unit'),
 
-    # Expense Forms
-    url(r'^(?P<business_unit>\d+)/expenses/create/$', views.ExpenseCreateView.as_view(), name='create_expense'),
-    url(r'^(?P<business_unit>\d+)/expenses/(?P<expense>\d+)/update/$', views.ExpenseUpdateView.as_view(), name='update_expense'),
-    url(r'^(?P<business_unit>\d+)/expenses/(?P<expense>\d+)/delete/$', views.ExpenseDeleteView.as_view(), name='delete_expense'),
-
-    # Cash Forms
-    url(r'^(?P<business_unit>\d+)/cash/(?P<month>\d+)-(?P<year>\d+)/$', views.CashCreateView.as_view(), name='create_cash'),
-    url(r'^(?P<business_unit>\d+)/cash/(?P<cash>\d+)/update/$', views.CashUpdateView.as_view(), name='update_cash'),
-
-    # Payroll Forms
-    url(r'^(?P<business_unit>\d+)/expense/(?P<month>\d+)-(?P<year>\d+)/create/$', views.PayrollExpenseCreateView.as_view(), name='create_payroll_expense'),
-
-    # Income Forms
-    url(r'^(?P<business_unit>\d+)/income/create/$', views.IncomeCreateView.as_view(), name='create_income'),
-    url(r'^(?P<business_unit>\d+)/income/(?P<income>\d+)/update/$', views.IncomeUpdateView.as_view(), name='update_income'),
-    url(r'^(?P<business_unit>\d+)/income/(?P<income>\d+)/delete/$', views.IncomeDeleteView.as_view(), name='delete_income'),
-
     # Contracts Forms
     url(r'^(?P<business_unit>\d+)/contracts/create/$', views.ContractCreateView.as_view(), name='create_contract'),
     url(r'^(?P<business_unit>\d+)/contracts/(?P<contract>\d+)/update/$', views.ContractUpdateView.as_view(), name='update_contract'),
