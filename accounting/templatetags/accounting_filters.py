64ad73ac1bb476db9f03f4c3d4084b28068be46e
Ryan P Kilby <rpkilby@ncsu.edu>
2017-02-06 20:49:19 -0500

    Refactor 'get_form_model_name()'

--- accounting/templatetags/accounting_filters.py
+++ accounting/templatetags/accounting_filters.py
@@ -2,8 +2,8 @@ from django.template.defaulttags import register
 
 
 @register.filter
-def get_form_model_name(model):
-    return model._meta.verbose_name.title()
+def get_form_model_name(form):
+    return form._meta.model._meta.verbose_name.title()
 
 
 @register.filter
