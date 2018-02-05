344dcf6e209933d4b99954cf584342ac59006f68
Ryan P Kilby <rpkilby@ncsu.edu>
2018-02-05 13:03:44 -0500

    Simplify app navs/cards

--- project/context_processors.py
+++ project/context_processors.py
@@ -2,7 +2,13 @@ from django.conf import settings
 
 
 def navbar_context(request):
+    app_name = getattr(request.resolver_match, 'app_name', None)
+    app = next((
+        app for app in settings.LABSITE_APPS
+        if app['app_name'] == app_name
+    ), None)
+
     return {
-        'current_app': getattr(request.resolver_match, 'app_name', None),
+        'current_app': app,
         'labsite_apps': settings.LABSITE_APPS,
     }
