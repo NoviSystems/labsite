6b009fdf9cec920ff1ba11d5cb73cffc8d2f1c6e
Ryan P Kilby <rpkilby@ncsu.edu>
2018-02-07 17:25:08 -0500

    Refactor settings

---
+++ project/deploy/settings.py
@@ -0,0 +1,22 @@
+'''
+-*- Production Settings -*-
+
+This file contains production-specific settings. Complete the deployment
+checklist and make any necessary changes.
+https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
+'''
+
+from ..common_settings import *
+
+
+SECURE_SSL_REDIRECT = True
+
+SESSION_COOKIE_SECURE = True
+
+CSRF_COOKIE_SECURE = True
+
+
+# manifest storage is useful for its automatic cache busting properties
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
+STATIC_ROOT = path('../static-root')
---
+++ project/settings.py
@@ -0,0 +1,15 @@
+'''
+-*- Development Settings -*-
+
+This file contains development-specific settings. You can run the django
+development server without making any changes to this file, but it's not
+suitable for production. The production settings files are located under
+the './deploy' directory.
+'''
+
+from .common_settings import *
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+SITE_URL = socket.gethostname()
