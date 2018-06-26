7ff2f29b90bc82a7808d9ab0487669349507ed83
Ryan P Kilby <rpkilby@ncsu.edu>
2018-06-26 13:38:34 -0400

    Add tox config, test files

--- tests/settings.py
+++ tests/settings.py
@@ -16,13 +16,5 @@ DATABASES = {
 EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
 
 
-# WORKLOG SETTINGS
-WORKLOG_SEND_REMINDERS = True
-
-WORKLOG_SEND_REMINDERS_HOUR = 18
-
-WORKLOG_EMAIL_REMINDERS_EXPIRE_AFTER = 4
-
-
 # Logging
 LOGGING['root']['level'] = 'ERROR'
